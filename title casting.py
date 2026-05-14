2f88b9af9d4241e85ef5b119623dd44b1a6ebf8f
nayshakabrabro-lab <nayshakabrabro@gmail.com>
2026-05-13 21:52:00 -0700

    initial commit

---
+++ title casting.py
@@ -0,0 +1,20 @@
+name="penguin"
+age=15
+is_student=True
+weight=38.5
+print("Name:",name)
+print("data type of name is",type(name))
+print("age:",age)
+print("data type of age is",type(age))
+print("is_student:",is_student)
+print("data type of is_student is",type(is_student))
+print("weight:",weight)
+print("data type of weight is",type(weight))
+age = str(age)
+print(age)
+print("data type of age is",type(age))
+weight = int(weight)
+print(weight)
+print("data type of weight is",type(weight))
+
+
